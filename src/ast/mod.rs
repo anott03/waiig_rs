4696c08e2c3ec922a5a1cf7aca3f7784a57fe746
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-07-20 14:27:29 +0200

    feat(ast): to_string

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,6 +1,6 @@
 use crate::token::{get_literal, Token};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Identifier {
     pub token: Token,
     pub value: String,
@@ -10,12 +10,22 @@ impl Identifier {
     pub fn token_literal(&self) -> String {
         return get_literal(&self.token);
     }
+
+    pub fn to_string(&self) -> String {
+        return get_literal(&self.token);
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Expression {}
 
-#[derive(Debug)]
+impl Expression {
+    pub fn to_string(&self) -> String {
+        return String::from("expression");
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct LetStatement {
     pub token: Token,
     pub name: Identifier,
@@ -26,9 +36,16 @@ impl LetStatement {
     pub fn token_literal(&self) -> String {
         return get_literal(&self.token);
     }
+
+    pub fn to_string(&self) -> String {
+        if let Some(val) = &self.value {
+            return get_literal(&self.token) + " " + self.name.to_string().as_str() + " = " + val.to_string().as_str();
+        }
+        return get_literal(&self.token) + " " + self.name.to_string().as_str() + " = null";
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ReturnStatement {
     pub token: Token,
     pub return_val: Expression,
@@ -38,9 +55,25 @@ impl ReturnStatement {
     pub fn token_literal(&self) -> String {
         return get_literal(&self.token);
     }
+
+    pub fn to_string(&self) -> String {
+        return String::from("return statement");
+    }
 }
 
 #[derive(Debug)]
+pub struct ExpressionStatement {
+    pub token: Token,
+    pub expression: Expression,
+}
+
+impl ExpressionStatement {
+    pub fn token_literal(&self) -> String {
+        return get_literal(&self.token);
+    }
+}
+
+#[derive(Debug, Clone)]
 pub enum Statement {
     LetStatement(LetStatement),
     ReturnStatement(ReturnStatement),
@@ -50,8 +83,16 @@ impl Statement {
     pub fn token_literal(&self) -> String {
         return String::from("");
     }
+
+    pub fn to_string(&self) -> String {
+        return match self {
+            Statement::LetStatement(ls) => ls.to_string(),
+            Statement::ReturnStatement(rs) => rs.to_string(),
+        }
+    }
 }
 
+#[derive(Clone)]
 pub struct Program {
     pub statements: Vec<Statement>,
 }
@@ -62,5 +103,34 @@ impl Program {
             return self.statements.first().unwrap().token_literal();
         }
         return String::from("");
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut out = String::new();
+        self.statements.iter().for_each(|s| {
+            out += &s.to_string();
+        });
+        return out;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ast::*;
+
+    #[test]
+    fn string() {
+        let program = Program {
+            statements: vec![
+                Statement::LetStatement(LetStatement {
+                    token: Token::LET,
+                    name: Identifier { token: Token::IDENT(String::from("x")), value: String::from("x") },
+                    value: None,
+                }),
+            ],
+        };
+
+        println!("{}", program.clone().to_string());
+        assert_eq!(program.to_string(), String::from("let x = null"));
     }
 }

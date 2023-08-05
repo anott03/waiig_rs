c421ceddd72e410af8fbabcbe412c19680a087b6
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-08-05 15:12:44 -0400

    quick repl

---
+++ src/repl/mod.rs
@@ -0,0 +1,20 @@
+use std::io::{stdout, stdin, Write};
+use crate::lexer::Lexer;
+use crate::parser::Parser;
+use crate::evaluator::eval;
+
+pub fn run() -> std::io::Result<()>{
+    loop {
+        print!("> ");
+        let _ = stdout().flush();
+        let mut input = String::new();
+        stdin().read_line(&mut input)?;
+
+        let l = Lexer::new(input.as_str());
+        let mut p = Parser::new(l);
+        let program = p.parse_program().expect("error parsing program");
+        let obj = eval(crate::ast::Node::Program(program));
+
+        println!("{}", obj.inspect());
+    }
+}
